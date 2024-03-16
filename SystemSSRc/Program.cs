871954b1d7300c5faa7ff97428907b5354cd54ba
dipsomask <alexey12345672@yandex.ru>
2024-03-16 22:31:32 +0700

    reworking-with-abstract-class-and-two-daughter-classes

--- SystemSSRc/Program.cs
+++ SystemSSRc/Program.cs
@@ -1,116 +1,327 @@
 ﻿using System.Net;
 using System.Net.Sockets;
-using System.Text;
 
 
 
-TCPListenerSSRc Listener1 = new(80);
+ConsoleSSRc consoleSSRc = new ConsoleSSRc();
 
-Listener1.Start();
-
-TCPListenerSSRc Listener2 = new(5000);
-
-Listener2.Start();
+consoleSSRc.UsingConsole();
 
 
-await Listener1.WorkerWithClients();
 
-await Listener2.WorkerWithClients();
-
-
-Listener1.Stop();
-
-Listener2.Stop();
-
-
-internal class TCPListenerSSRc
+// modified TCPListener abstract class
+public abstract class Listener
 {
 
-    private readonly TcpListener listener;
 
-    private readonly int Port;
+    protected TcpListener listener;
+
+    protected int Port;
 
 
-    // make tcplistener object
-    public TCPListenerSSRc(int port)
+    public Listener(int port_ = 80)
     {
 
-        Port = port;
+        listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port_);
 
-        listener = new TcpListener(IPAddress.Parse("127.0.0.1"), Port);
+        Port = port_;
 
     }
 
 
-    // start listen connections to ssrc listener
-    public void Start()
+    public void StartListen()
     {
 
         listener.Start();
 
-        Console.WriteLine("SSRc is listening and awating connection...");
-
     }
 
 
-    // stop listen connections to ssrc listener
-    public void Stop()
+    public void StopListen()
     {
 
         listener.Stop();
 
-        Console.WriteLine("SSRc finished listen...");
-
     }
 
 
-    // make client and work with his request
-    public async Task WorkerWithClients()
+    public abstract void ListenProcess();
+
+
+
+    // methodes for SystemListenerSSRc
+    public virtual void MakeUser() {  }
+
+
+    public virtual void StopUser() {  }
+
+
+    public virtual void DeleteUser() {  }
+
+
+    public virtual void AddPath(string arg1, string arg2) {  }
+
+
+    public virtual void RemovePath(string _) {  }
+
+
+    public virtual void GetPath(string _) { }
+
+
+
+    // methodes for UserListenerSSRc
+    public virtual void RunProcess(object? _) { }
+
+    public virtual void DownloadObject(object? _) {  }
+
+    public virtual byte? ImportObject(object? _) { return null; }
+
+
+
+}
+
+
+
+// system listener class
+public class SystemListenerSSRc : Listener
+{
+
+
+    public override async void ListenProcess()
     {
 
         while (true)
         {
 
-            TcpClient tcpClient = await listener.AcceptTcpClientAsync();
+            TcpClient Client = await listener.AcceptTcpClientAsync();
 
-            await ResponseMessage(tcpClient, "Server listened you!");
+            
+
+
 
         }
 
     }
 
 
-    //test function of request-response string
-    public async Task ResponseMessage(TcpClient tcpClient, string message)
+    public override void MakeUser()
     {
-
-        var _ = tcpClient;
-
-        var stream = tcpClient.GetStream();
+        Console.WriteLine("ok");
+    }
 
 
-        // read data
-        byte[] sizeBuffer = new byte[4];
-
-        await stream.ReadAsync(sizeBuffer, 0, sizeBuffer.Length);
-
-        byte[] responseData = new byte[BitConverter.ToInt32(sizeBuffer, 0)];
-
-        int bytes = await stream.ReadAsync(responseData, 0, responseData.Length);
-
-        string response = Encoding.UTF8.GetString(responseData);
-
-        Console.WriteLine(response);
+    public override void StopUser()
+    {
+        Console.WriteLine("ok");
+    }
 
 
-        //make answer and send
-        byte[] dataAnswer = Encoding.UTF8.GetBytes(message);
-
-        await stream.WriteAsync(dataAnswer, 0, dataAnswer.Length);
-
-        Console.WriteLine("Successful answer");
+    public override void DeleteUser()
+    {
+        Console.WriteLine("ok");
     }
 
 
 
+    // console methodes
+    public override void AddPath(string arg1, string arg2)
+    {
+        Console.WriteLine("ok");
+    }
+
+
+    public override void RemovePath(string _)
+    {
+        Console.WriteLine("ok");
+    }
+
+
+    public override void GetPath(string _)
+    {
+        Console.WriteLine("ok");
+    }
+
+
 }
+
+
+
+// users connections listener class
+public class UserListenerSSRc(int port_) : Listener(port_)
+{
+
+
+    public override async void ListenProcess()
+    {
+
+        while (true)
+        {
+
+            TcpClient Client = await listener.AcceptTcpClientAsync();
+
+
+
+
+
+        }
+
+    }
+
+
+    public override void RunProcess(object? _)
+    {
+
+        Console.WriteLine("ok");
+
+    }
+
+    public override void DownloadObject(object? _)
+    {
+
+        Console.WriteLine("ok");
+
+    }
+
+    public override byte? ImportObject(object? _) 
+    {
+
+        Console.WriteLine("ok");
+
+        return null;
+    
+    }
+
+
+}
+
+
+
+internal class ConsoleSSRc
+{
+
+
+    /*console comands        
+    {"add", "-pa" },
+    {"remove", "-pa" },
+    {"find", "-pa"},
+    */
+
+    private SystemListenerSSRc systemL;
+
+
+    public ConsoleSSRc()
+    {
+
+        systemL = new SystemListenerSSRc();
+
+        Console.WriteLine("console ready to work...");
+
+    }
+
+
+    public async void UsingConsole()
+    {
+
+        while (true)
+        {
+
+            string? consoleInput = await Console.In.ReadLineAsync();
+
+
+            if (consoleInput != null)
+            {
+
+                string[] consoleInputArray= consoleInput.Split(' ');
+
+                switch (consoleInputArray[0])
+                {
+
+                    case "add":
+
+                        if(consoleInputArray.Length > 2 && consoleInputArray.Length < 5)
+                        {
+
+                            if (consoleInputArray[1] == "-pa")
+                            {
+
+                                systemL.AddPath(consoleInputArray[2], consoleInputArray[3]);
+
+                            }
+                            else
+                            {
+
+                                Console.WriteLine("It is not an internal or external command...");
+
+                            }
+
+                        }
+
+                        break;
+
+
+
+                    case "remove":
+
+                        if (consoleInputArray.Length == 3)
+                        {
+
+                            if (consoleInputArray[1] == "-pa")
+                            {
+
+                                systemL.RemovePath(consoleInputArray[2]);
+
+                            }
+                            else
+                            {
+
+                                Console.WriteLine("It is not an internal or external command...");
+
+                            }
+
+                        }
+
+                        break;
+
+                    case "find":
+
+                        if( consoleInputArray.Length == 3)
+                        {
+
+                            if (consoleInputArray[1] == "-pa")
+                            {
+
+                                systemL.GetPath(consoleInputArray[2]);
+
+                            }
+                            else
+                            {
+
+                                Console.WriteLine("It is not an internal or external command...");
+
+                            }
+
+                        }
+
+                        break;
+
+
+                    default:
+
+                        Console.WriteLine("It is not an internal or external command...");
+
+                        break;
+
+                }
+
+            }
+
+
+        }
+
+
+
+    }
+
+
+}
+
